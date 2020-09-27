f40a31eb075fe1421fb75b377ba0c6116ab79ffb
joelLopezArias <70671117+joelLopezArias@users.noreply.github.com>
2020-09-26 19:58:40 -0500

    version 1

---
+++ js/main.js
@@ -0,0 +1,19 @@
+$(document).ready(function() {
+    $(window).scroll(function() {
+        if (this.scrollY > 20) {
+            $(".navbar").addClass("sticky");
+        } else {
+            $(".navbar").removeClass("sticky");
+        }
+    })
+
+    //plugin magnific popup
+    $('.works').magnificPopup({
+        delegate: 'a',
+        type: 'image',
+        gallery: {
+            enabled: true,
+            preload: [0.2]
+        }
+    });
+});
